bfeee13293cc34c77eeae5e8bacc8152b650d9f8
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-11 23:10:04 +0300

    update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-        "extends": [
-            "airbnb-base",
-            "prettier"
-        ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-param-reassign": 0,
-        "no-plusplus": 0,
-        "import/no-cycle": [0, { maxDepth: 1 }],
-        "import/prefer-default-export": 0
-      }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+    "extends": [
+      "airbnb-base",
+      "prettier"
+    ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-param-reassign": 0,
+    "no-plusplus": 0,
+    "import/no-cycle": [0, { maxDepth: 1 }],
+    "import/prefer-default-export": 0
+  }
 };
