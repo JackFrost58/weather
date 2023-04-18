01d1cb84b40c0a5db2bd3f01d114baa4044caecd
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-18 23:27:24 +0300

    sidebar: add template

---
+++ weather-app/src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {routerButtonConfig, settingButtonConfig} from './constants/buttons-config.constant';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  public readonly routerButtons = routerButtonConfig;
+  public readonly settingButtons = settingButtonConfig;
+}
