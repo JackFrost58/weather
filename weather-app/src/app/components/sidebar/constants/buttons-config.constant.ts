0ef6b0c43ea3554b5d9feccda04e2c9dec9d38d9
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-18 23:28:36 +0300

    add buttons config constant and interface

---
+++ weather-app/src/app/components/sidebar/constants/buttons-config.constant.ts
@@ -0,0 +1,31 @@
+import {ButtonConfig} from "../interfaces/button.interface"
+
+export const routerButtonConfig: ButtonConfig[] = [
+  {
+    title: 'Dashboard',
+    icon: 'home'
+  },
+  {
+    title: 'Map',
+    icon: 'map'
+  },
+  {
+    title: 'Saved Location',
+    icon: 'local_library'
+  },
+  {
+    title: 'Calendar',
+    icon: 'calendar_monthCalendar'
+  },
+]
+
+export const settingButtonConfig: ButtonConfig[] = [
+  {
+    title: 'Settings',
+    icon: 'settings'
+  },
+  {
+    title: 'Logout account',
+    icon: 'logout'
+  },
+]
---
+++ weather-app/src/app/components/sidebar/interfaces/button.interface.ts
@@ -0,0 +1,4 @@
+export interface ButtonConfig {
+  title: string;
+  icon: string;
+}
