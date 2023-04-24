1cd17145d700531f77aa47a40d6c86ffb8833a91
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-24 19:30:09 +0300

    weather-info: fix style and template

--- weather-app/src/app/pages/dashboard/components/weather-info/weather-info.module.ts
+++ weather-app/src/app/pages/dashboard/components/weather-info/weather-info.module.ts
@@ -1,16 +1,18 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {MaterialModule} from '@shared/material.module';
+import {TemperatureChartModule} from '../temperature-chart/temperature-chart.module';
 import {WeatherInfoComponent} from './weather-info.component';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    TemperatureChartModule
   ],
   declarations: [WeatherInfoComponent],
   exports: [WeatherInfoComponent]
