ca65c3204e5756559b2a13e01eb0f8c1b07d15d7
Vladislav Zhgirov <zgv2324@gmail.com>
2023-04-26 23:01:21 +0300

    detailde-weather: add template

--- weather-app/src/app/pages/dashboard/components/detailed-weather/detailed-weather.component.ts
+++ weather-app/src/app/pages/dashboard/components/detailed-weather/detailed-weather.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {faSun} from '@fortawesome/free-solid-svg-icons';
+import {faCloud} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'detailed-weather',
@@ -6,7 +8,35 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./detailed-weather.component.scss']
 })
 export class DetailedWeatherComponent implements OnInit {
-
+  public faCloud = faCloud;
+  public dayStateConfig = [
+    {
+      title: 'Sunrise',
+      icon: 'cloudy'
+    },
+    {
+      title: 'Sunset',
+      icon: 'sunny_snowing'
+    }
+  ]
+  public items = [
+    {
+      time: '7 PM',
+      progress: 44
+    },
+    {
+      time: '8 PM',
+      progress: 30
+    },
+    {
+      time: '9 PM',
+      progress: 67
+    },
+    {
+      time: '10 PM',
+      progress: 72
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- weather-app/src/app/pages/dashboard/components/detailed-weather/detailed-weather.module.ts
+++ weather-app/src/app/pages/dashboard/components/detailed-weather/detailed-weather.module.ts
@@ -1,10 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {DetailedWeatherComponent} from './detailed-weather.component';
+import {MaterialModule} from '@shared/material.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
+    MaterialModule
   ],
   declarations: [DetailedWeatherComponent],
   exports: [DetailedWeatherComponent]
