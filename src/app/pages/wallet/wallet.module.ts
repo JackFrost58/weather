07a398d6555515f107724b4d8c2122d3c99490a1
Vladislav Zhgirov <zgv2324@gmail.com>
2023-07-25 15:54:37 +0300

    wallet: add header module

--- src/app/pages/wallet/wallet.module.ts
+++ src/app/pages/wallet/wallet.module.ts
@@ -2,11 +2,15 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {WalletComponent} from './wallet.component';
 import {WalletRouterModule} from './wallet.router';
+import {HeaderInfoModule} from '@shared/components/header-info/header-info.module';
+import {MaterialModule} from '@shared/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    WalletRouterModule
+    WalletRouterModule,
+    HeaderInfoModule,
+    MaterialModule
   ],
   declarations: [WalletComponent],
   exports: [WalletComponent]
