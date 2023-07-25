6a6a04f611d83422c7d294aa5cccda56e9b27d55
Vladislav Zhgirov <zgv2324@gmail.com>
2023-07-25 15:54:21 +0300

    health: add header module

--- src/app/pages/health/health.module.ts
+++ src/app/pages/health/health.module.ts
@@ -1,12 +1,16 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {HeaderInfoModule} from '@components/header-info/header-info.module';
+import {MaterialModule} from '@shared/material.module';
 import {HealthComponent} from './health.component';
 import {HealthRouterModule} from './health.router';
 
 @NgModule({
   imports: [
     CommonModule,
-    HealthRouterModule
+    HealthRouterModule,
+    HeaderInfoModule,
+    MaterialModule
   ],
   declarations: [HealthComponent],
   exports: [HealthComponent]
