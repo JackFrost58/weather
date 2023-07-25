2ad60ad3478a289585d87d1a32a509669148970c
Vladislav Zhgirov <zgv2324@gmail.com>
2023-07-25 15:54:29 +0300

    map: add header module

--- src/app/pages/map/map.module.ts
+++ src/app/pages/map/map.module.ts
@@ -2,11 +2,15 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MapComponent} from './map.component';
 import {MapRouterModule} from './map.router';
+import {HeaderInfoModule} from '@shared/components/header-info/header-info.module';
+import {MaterialModule} from '@shared/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MapRouterModule
+    MapRouterModule,
+    HeaderInfoModule,
+    MaterialModule
   ],
   declarations: [MapComponent]
 })
