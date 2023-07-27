1325ce369946602ab28c50b874dbd3139e0f7b9f
Vladislav Zhgirov <zgv2324@gmail.com>
2023-07-27 12:09:18 +0300

    app: rename router file and add input in sidebar template element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatNativeDateModule} from '@angular/material/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule} from './app.routing';
 import {AppComponent} from './app.component';
 import {SidebarModule} from './components/sidebar/sidebar.module';
 import {MaterialModule} from './shared/material.module';
